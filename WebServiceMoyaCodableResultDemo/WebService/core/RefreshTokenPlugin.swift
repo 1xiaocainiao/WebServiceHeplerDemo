5246e5bee129c3762a9f6986d35fbc5ef47d36e9
mac <>
2024-10-21 15:43:05 +0800

    添加是否需要刷新判断

--- WebServiceMoyaCodableResultDemo/WebService/core/RefreshTokenPlugin.swift
+++ WebServiceMoyaCodableResultDemo/WebService/core/RefreshTokenPlugin.swift
@@ -15,6 +15,8 @@ public class RefreshTokenPlugin: PluginType {
     
     private var semaphore = DispatchSemaphore(value: 0)
     
+    private var isRefreshing = false
+    
     public init() {
         
     }
@@ -34,45 +36,51 @@ public class RefreshTokenPlugin: PluginType {
         // if less than 1 hour, refresh token.
         if (TokenManager.shared.expiredTimestamp - now) < 3600, let refreshToke = TokenManager.shared.refreshToken  {
             
-            NSLog("start refresh token automatic", "")
-            
-            let provider = MoyaProvider<SRAccountApi>()
-            
-            // refresh token once
-            provider.request(.refreshToken(refreshToken: refreshToke), callbackQueue: DispatchQueue.global()) { result in
+            if !isRefreshing {
+                isRefreshing = true
                 
-                defer {
-                    self.semaphore.signal()
-                }
+                NSLog("start refresh token automatic", "")
                 
-                do {
-
-                    let response = try result.get()
-                    let value = try response.mapJSON() as? [String: Any]
-
-                    if let code = value?["code"] as? String,
-                       let message = value?["message"] as? String,
-                       code == "10001",
-                       let data = value?["data"] as? [String: Any],
-                       let authorization = data["authorization"] as? [String: Any] {
-
-                        print("refresh token successful! \(code) \(message) \(data)")
+                let provider = MoyaProvider<SRAccountApi>()
+                
+                // refresh token once
+                provider.request(.refreshToken(refreshToken: refreshToke), callbackQueue: DispatchQueue.global()) { result in
+                    
+                    defer {
+                        isRefreshing = false
                         
-                        // Update tokens and expired timestamp.
-                        TokenManager.shared.accessToken = authorization["accessToken"] as? String ?? ""
-                        TokenManager.shared.refreshToken = authorization["refreshToken"] as? String
-                        TokenManager.shared.expiredTimestamp = (authorization["expiredTimestamp"] as? TimeInterval ?? 0) / 1000
-                        
-                    } else {
-                        
-                        print("refresh token failed!")
+                        self.semaphore.signal()
+                    }
+                    
+                    do {
+
+                        let response = try result.get()
+                        let value = try response.mapJSON() as? [String: Any]
+
+                        if let code = value?["code"] as? String,
+                           let message = value?["message"] as? String,
+                           code == "10001",
+                           let data = value?["data"] as? [String: Any],
+                           let authorization = data["authorization"] as? [String: Any] {
+
+                            print("refresh token successful! \(code) \(message) \(data)")
+                            
+                            // Update tokens and expired timestamp.
+                            TokenManager.shared.accessToken = authorization["accessToken"] as? String ?? ""
+                            TokenManager.shared.refreshToken = authorization["refreshToken"] as? String
+                            TokenManager.shared.expiredTimestamp = (authorization["expiredTimestamp"] as? TimeInterval ?? 0) / 1000
+                            
+                        } else {
+                            
+                            print("refresh token failed!")
+                            TokenManager.shared.refreshToken = nil
+                        }
+
+                    } catch {
+
+                        NSLog("error %@", error.localizedDescription)
                         TokenManager.shared.refreshToken = nil
                     }
-
-                } catch {
-
-                    NSLog("error %@", error.localizedDescription)
-                    TokenManager.shared.refreshToken = nil
                 }
             }
             
